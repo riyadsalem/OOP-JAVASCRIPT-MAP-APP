ba13fadc0e0edb03d3509748d4929264de72006a
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-07-04 12:37:57 +0300

    GET Geolocation API

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/*
+/////// Simple project analysis stages ///////
+USER STORY
+FEATURES
+FLOWCHART
+ARCHITECTURE
+*/
+
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -10,3 +18,14 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    // console.log(position.coords.latitude);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    // console.log(latitude, longitude);
+    console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+  },
+  function () {}
+);
