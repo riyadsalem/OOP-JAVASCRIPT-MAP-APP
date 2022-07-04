46860bf2b80637f4b81fdae3667d3914a9cd6dce
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-07-04 14:26:40 +0300

    Displaying a Map Marker

--- script.js
+++ script.js
@@ -22,10 +22,9 @@ const inputElevation = document.querySelector('.form__input--elevation');
 navigator.geolocation.getCurrentPosition(
   function (position) {
     // fucntion callback Pass
-    // console.log(position.coords.latitude);
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    // console.log(latitude, longitude);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
     console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
 
     const coords = [latitude, longitude];
@@ -33,16 +32,33 @@ navigator.geolocation.getCurrentPosition(
     /////
     //  id="map"
     const map = L.map('map').setView(coords, 13);
+    //console.log(typeof map, map);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker(coords)
-      .addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
+    map.on('click', function (mapEvent) {
+      //  console.log(mapEvent);
+
+      const { lat, lng } = mapEvent.latlng;
+
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+      // console.log(lat, lng);
+    });
 
     /////
   },
